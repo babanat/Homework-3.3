ebc2f5e32d690e34e9c7f5af2593918a28371467
babanat <babanat3@gmail.com>
2024-06-19 16:57:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-const spacedNumbers = `1 0 3 6 9`;
-
-console.log(spacedNumbers);
+const one = 1;
+const zero = 0;
+const three = 3;
+const six = 6;
+const nine = 9;
+const spacedNumbers = (`${one} ${zero} ${three} ${six} ${nine}`);
+console.log(spacedNumbers); 
+/* Sorry i have this solution in my head -1*/
+console.log(`${1 + ' ' + 0 + ' ' + 3 + ' ' + 6 + ' ' + 9 }`);
+/* Or i have some another solution */
